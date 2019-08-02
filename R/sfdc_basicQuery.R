d3eb4020a461875c45960aef32827600a1d58431
Jack Serna <jackdserna@gmail.com>
2019-08-02 11:51:21 -0700

    Impeccably TRUE

--- R/sfdc_basicQuery.R
+++ R/sfdc_basicQuery.R
@@ -1,43 +1,42 @@
 # Function for basic querying
 sfdc_basicQuery<- function(session, object, query) {
-   # If missing args, stop
-   if(missing(session)) stop("Login to Salesforce with sfdc_login()")
-   if(missing(object) | missing(query)) stop("Provide an object and query")
-   # Run
-   job<- rforcecom.createBulkJob(
-      session,
-      operation = 'query',
-      object = object)
-   info<- rforcecom.submitBulkQuery(
-      session,
-      jobId = job$id,
-      query = query)
-   repeat{
-      status<- suppressWarnings(rforcecom.checkBatchStatus(
-         session,
-         jobId = info$jobId,
-         batchId = info$id))
-      if (status[["state"]] == "Completed") {
-         break
-      }
-      if (status[["state"]] == "Failed") {
-         errorMessage = status$stateMessage
-         break
-      }
-   }
-   Sys.sleep(0.01)
-   details<- suppressWarnings(rforcecom.getBatchDetails(
-      session,
-      jobId = info$jobId,
-      batchId = info$id))
-   result<- suppressWarnings(rforcecom.getBulkQueryResult(
-      session,
-      jobId = info$jobId,
-      info$id,
-      resultId = details$result))
-   closeJob<- rforcecom.closeBulkJob(
-      session,
-      jobId = job$id)
-   if(exists("errorMessage")) stop(errorMessage)
-   return(result)
+        # If missing args, stop
+        if(missing(session)) stop("Login to Salesforce with sfdc_login()")
+        if(missing(object) | missing(query)) stop("Provide an object and query")
+        # Run
+        job<- RForcecom::rforcecom.createBulkJob(
+                session,
+                operation = 'query',
+                object = object)
+        info<- RForcecom::rforcecom.submitBulkQuery(
+                session,
+                jobId = job$id,
+                query = query)
+        repeat{
+                status<- suppressWarnings(RForcecom::rforcecom.checkBatchStatus(
+                        session,
+                        jobId = info$jobId,
+                        batchId = info$id))
+                if (status[["state"]] == "Completed") {
+                        break
+                }
+                if (status[["state"]] == "Failed") {
+                        errorMessage = status$stateMessage
+                        break
+                }
+        }
+        details<- suppressWarnings(RForcecom::rforcecom.getBatchDetails(
+                session,
+                jobId = info$jobId,
+                batchId = info$id))
+        result<- suppressWarnings(RForcecom::rforcecom.getBulkQueryResult(
+                session,
+                jobId = info$jobId,
+                info$id,
+                resultId = details$result))
+        closeJob<- RForcecom::rforcecom.closeBulkJob(
+                session,
+                jobId = job$id)
+        if(exists("errorMessage")) stop(errorMessage)
+        result
 }
