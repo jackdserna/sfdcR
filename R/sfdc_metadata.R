5311d7e66c6d3f7f0d36545f377610df7511ab9c
Jack Serna <jackdserna@gmail.com>
2019-08-06 16:52:15 -0700

    ready for plotting, user-testing

--- R/sfdc_metadata.R
+++ R/sfdc_metadata.R
@@ -1,9 +1,13 @@
 # Extract metadata from all Salesforce Objects
 # Specific field types whill have all relevant information included in the data set. 
 # Otherwise, all field types will include only general information.
-sfdc_meta<- function(session, objects_metadata = NULL, field_type = "all") {
+sfdc_meta<- function(session, objects = NULL, field_type = "all") {
         if(missing(session)) stop("Login to Salesforce with sfdc_login()")
-        if(missing(objects_metadata)) assign("objects", RForcecom::rforcecom.getObjectList(session))
+        if(missing(objects)) {
+                assign("objects", RForcecom::rforcecom.getObjectList(session));
+                objects<- objects[,c("name", "label")]
+                objects[]<- lapply(objects, as.character)
+        }
         SFDC_arch<- data.frame()
         cols = c("parent_object",
                  "referenceTo",
@@ -15,6 +19,10 @@ sfdc_meta<- function(session, objects_metadata = NULL, field_type = "all") {
                  "permissionable",
                  "nillable",
                  "restrictedPicklist") # Standard metadata
+        switch(field_type,
+               "picklist" = assign("cols", cols[!cols %in% c("calculated", "referenceTo")]),
+               "formula" = assign("cols", cols[!cols %in% c("restrictedPicklist", "referenceTo")]),
+               "reference" = assign("cols", cols[!cols %in% c("calculated", "restrictedPicklist")]))
         collect<- function(data, index, fields, ...){
                 data[, fields[1]]<- index # Create column "parent_object" and value is equal to object name
                 data<- data[, fields]
@@ -27,26 +35,27 @@ sfdc_meta<- function(session, objects_metadata = NULL, field_type = "all") {
                 # field type may be set equal to "picklist" for only picklist fields and picklist values
                 # field type defaults to all types, and will provide only 1 referenceTo object
                 if(!is.null(colnames(data)) & any(grepl("referenceTo", colnames(data))==TRUE)){
+                        # Filter data for specific type if applicable
                         switch(field_type,
-                               "picklist" = assign("fields",
-                                                   append(cols,
-                                                          colnames(data)[grep("picklistValues.value",
-                                                                              colnames(data))])),
-                               "formula" = assign("fields",
-                                                  append(cols,
-                                                         colnames(data)[grep("calculatedFormula",
-                                                                             colnames(data))])),
-                               "reference" = assign("fields",
+                               "picklist" = assign("data", data[data[ , "type"] == field_type, ]),
+                               "formula" = assign("data", data[data[ , "calculated"] == "true", ]),
+                               "reference" = assign("data", data[data[ , "type"] == field_type, ]))
+                        if(nrow(data) > 0) { collect(data, index, fields = switch(
+                                field_type,
+                                "picklist" = assign("fields",
                                                     append(cols,
-                                                           colnames(data)[grep("referenceTo",
+                                                           colnames(data)[grep("picklistValues.value",
                                                                                colnames(data))])),
-                               "all" = assign("fields", cols))
-                               # Filter data for specific type if applicable
-                               switch(field_type,
-                                      "picklist" = assign("data", data[, "type" == field_type]),
-                                      "formula" = assign("data", data[, "calculated" == "true"]),
-                                      "reference" = assign("data", data[, "type" == field_type]))
-                               if(nrow(data)>0) collect(data, index, fields)
+                                "formula" = assign("fields",
+                                                   append(cols,
+                                                          colnames(data)[grep("calculatedFormula",
+                                                                              colnames(data))])),
+                                "reference" = assign("fields",
+                                                     append(cols,
+                                                            colnames(data)[grep("referenceTo",
+                                                                                colnames(data))])),
+                                "all" = assign("fields", cols))
+                        )}
                 }
         }
         SFDC_arch<- lapply(objects$name, FUN = function(x) {
@@ -54,10 +63,17 @@ sfdc_meta<- function(session, objects_metadata = NULL, field_type = "all") {
                                                  index = x,
                                                  field_type = field_type)})
         })
-        data<- data.table::rbindlist(lapply(SFDC_arch, data.table::rbindlist), fill = TRUE)
-        if(ncol(data) > 10) {
+        data<- data.table::rbindlist(lapply(SFDC_arch, FUN = function(x) {
+                data.table::rbindlist(x, fill = TRUE)}), fill = TRUE)
+        if(ncol(data) > length(cols)) {
+                switch(field_type,
+                       "picklist" = assign("cols", cols[!grepl("picklistValues.value", cols)]),
+                       "reference" = assign("cols", cols[!grepl("referenceTo", cols)]))
                 data <- data.table::melt(data, id.vars = cols) %>%
-                        dplyr::drop_na(value)
+                        tidyr::drop_na(value) %>%
+                        group_by(parent_object, name, value) %>%
+                        slice(1) %>%
+                        ungroup()
         }
         data
 }
